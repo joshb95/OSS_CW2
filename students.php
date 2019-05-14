87658df56494e76a3d85400f51c3cc80abf50dd0
Josh <joshjbarnes@hotmail.co.uk>
2019-05-14 15:02:01 +0100

    Task 3

--- students.php
+++ students.php
@@ -36,3 +36,58 @@
    echo template("templates/partials/footer.php");
 
 ?>
+
+<script type="text/javascript" src="js/jquery-1.6.2.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#checkBoxAll').click(function() {
+			if ($(this).is(":checked"))
+				$('.chkCheckBoxId').prop('checked', true);
+			else
+				$('.chkCheckBoxId').prop('checked', false);
+		});
+	});
+</script>
+<?php
+
+if(isset($_POST['buttonDelete'])) {
+	if(isset($_POST['studentid'])) {
+		foreach ($_POST['studentid'] as $sudentid) {
+			$stmt = $conn->prepare('delete from student where studentid = :studentid');
+			$stmt->bindValue('studentid', $studentid);
+			$stmt->execute();
+		}
+	}
+}
+$stmt = $conn->prepare('select * from student');
+$stmt->execute();
+?>
+<form method="post" action="index.php">
+	<input type="submit" name="buttonDelete" value="Delete" onclick="return confirm('Are you sure?')" />
+	<table cellpadding="2" cellspacing="2" border="1">
+		<tr>
+			<th><input type="checkbox" id="checkBoxAll" /></th>
+			<th>ID</th>
+			<th>DOB</th>
+			<th>FirstName</th>
+      <th>Surname</th>
+      <th>House</th>
+      <th>Town</th>
+      <th>County</th>
+      <th>Country</th>
+		</tr>
+		<?php while($studentid = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
+		<tr>
+			<td><input type="checkbox" class="chkCheckBoxId"
+				value="<?php echo $studentid->studentid; ?>" name="studentid[]" /></td>
+			<td><?php echo $studentid->dob; ?></td>
+			<td><?php echo $studentid->firstname; ?></td>
+			<td><?php echo $studentid->lastname; ?></td>
+      <td><?php echo $studentid->house; ?></td>
+      <td><?php echo $studentid->town; ?></td>
+      <td><?php echo $studentid->county; ?></td>
+      <td><?php echo $studentid->country; ?></td>
+		</tr>
+		<?php } ?>
+	</table>
+</form>
