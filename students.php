b7c49e69c5d4681475de9189f51ce3d3d1123273
Josh <joshjbarnes@hotmail.co.uk>
2019-05-14 14:46:29 +0100

    Task 3

--- students.php
+++ students.php
@@ -4,6 +4,12 @@
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
 
+   $checkbox = $_POST['check'];
+ 	for($i=0;$i<count($checkbox);$i++){
+ 	$del_id = $checkbox[$i];
+ 	mysqli_query($conn,"DELETE FROM employee WHERE userid='".$del_id."'");
+ 	$message = "Data deleted successfully !";
+ }
    // check logged in
    if (isset($_SESSION['id'])) {
 
@@ -25,7 +31,7 @@
       }
       $data['content'] .= "</table>";
 
-    
+
       // render the template
       echo template("templates/default.php", $data);
 
@@ -36,3 +42,64 @@
    echo template("templates/partials/footer.php");
 
 ?>
+
+<?php
+include_once 'database.php';
+if(isset($_POST['save'])){
+	$checkbox = $_POST['check'];
+	for($i=0;$i<count($checkbox);$i++){
+	$del_id = $checkbox[$i];
+	mysqli_query($conn,"DELETE FROM employee WHERE userid='".$del_id."'");
+	$message = "Data deleted successfully !";
+}
+}
+$result = mysqli_query($conn,"SELECT * FROM employee");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<title>Delete employee data</title>
+</head>
+<body>
+<div><?php if(isset($message)) { echo $message; } ?>
+</div>
+<form method="post" action="">
+<table class="table table-bordered">
+<thead>
+<tr>
+    <th><input type="checkbox" id="checkAl"> Select All</th>
+	<th>Employee Id</th>
+	<th>First Name</th>
+	<th>Last Name</th>
+	<th>City</th>
+	<th>Email id</th>
+</tr>
+</thead>
+<?php
+$i=0;
+while($row = mysqli_fetch_array($result)) {
+?>
+<tr>
+    <td><input type="checkbox" id="checkItem" name="check[]" value="<?php echo $row["userid"]; ?>"></td>
+	<td><?php echo $row["userid"]; ?></td>
+	<td><?php echo $row["first_name"]; ?></td>
+	<td><?php echo $row["last_name"]; ?></td>
+	<td><?php echo $row["city_name"]; ?></td>
+	<td><?php echo $row["email"]; ?></td>
+</tr>
+<?php
+$i++;
+}
+?>
+</table>
+<p align="center"><button type="submit" class="btn btn-success" name="save">DELETE</button></p>
+</form>
+<script>
+$("#checkAl").click(function () {
+$('input:checkbox').not(this).prop('checked', this.checked);
+});
+</script>
+</body>
+</html>
