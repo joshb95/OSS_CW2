2bbd389881612c3374f50c686f3c969569102270
Josh <joshjbarnes@hotmail.co.uk>
2019-05-21 11:02:22 +0100

    sql injection

--- addstudent.php
+++ addstudent.php
@@ -13,6 +13,16 @@ $county = $_POST['County'];
 $country = $_POST['Country'];
 $postcode = $_POST['Postcode'];
 
+$id = $mysqli->real_escape_string($id);
+$DOB = $mysqli->real_escape_string($DOB);
+$fn = $mysqli->real_escape_string($fn);
+$ln = $mysqli->real_escape_string($ln);
+$house = $mysqli->real_escape_string($house);
+$town = $mysqli->real_escape_string($town);
+$county = $mysqli->real_escape_string($county);
+$country = $mysqli->real_escape_string($country);
+$postcode = $mysqli->real_escape_string($postcode);
+
 $sql = "INSERT INTO Student (studentid, dob, firstname, lastname, house, town, county, country, postcode)
 VALUES ('$id', '$DOB', '$fn', '$ln', '$house', '$town', '$county', '$country', '$postcode')";
 if (mysqli_query($conn, $sql)) {
