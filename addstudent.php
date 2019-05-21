50c0528dce9a020fc0a49879d28c9190dc7f76ce
Josh <joshjbarnes@hotmail.co.uk>
2019-05-21 12:13:49 +0100

    Fix

--- addstudent.php
+++ addstudent.php
@@ -13,17 +13,6 @@ $county = $_POST['County'];
 $country = $_POST['Country'];
 $postcode = $_POST['Postcode'];
 
-$id = $mysqli->real_escape_string($conn, $id);
-$DOB = $mysqli->real_escape_string($conn, $DOB);
-$fn = $mysqli->real_escape_string($conn, $fn);
-$ln = $mysqli->real_escape_string($conn, $ln);
-$house = $mysqli->real_escape_string($conn, $house);
-$town = $mysqli->real_escape_string($conn, $town);
-$county = $mysqli->real_escape_string($conn, $county);
-$country = $mysqli->real_escape_string($conn, $country);
-$postcode = $mysqli->real_escape_string($conn, $postcode);
-
-
 
 $sql = "INSERT INTO Student (studentid, dob, firstname, lastname, house, town, county, country, postcode)
 VALUES ('$id', '$DOB', '$fn', '$ln', '$house', '$town', '$county', '$country', '$postcode')";
