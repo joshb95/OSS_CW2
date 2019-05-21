f9ec64f6685d5cf74460e9f8817a690fa5b4e462
Josh <joshjbarnes@hotmail.co.uk>
2019-05-21 12:53:46 +0100

    SQL Injection

--- addstudent.php
+++ addstudent.php
@@ -3,26 +3,15 @@ include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 // check logged in
-$id = $_POST['ID'];
-$DOB = $_POST['DOB'];
-$fn = $_POST['FirstName'];
-$ln = $_POST['Surname'];
-$house = $_POST['House'];
-$town = $_POST['Town'];
-$county = $_POST['County'];
-$country = $_POST['Country'];
-$postcode = $_POST['Postcode'];
-
-$id = $mysqli->real_escape_string($conn, $id);
-$DOB = $mysqli->real_escape_string($conn, $DOB);
-$fn = $mysqli->real_escape_string($conn, $fn);
-$ln = $mysqli->real_escape_string($conn, $ln);
-$house = $mysqli->real_escape_string($conn, $house);
-$town = $mysqli->real_escape_string($conn, $town);
-$county = $mysqli->real_escape_string($conn, $county);
-$country = $mysqli->real_escape_string($conn, $country);
-$postcode = $mysqli->real_escape_string($conn, $postcode);
-
+$id = mysqli_real_escape_string($conn, $_POST['ID'] );
+$DOB = mysqli_real_escape_string($conn, $_POST['DOB']);
+$fn = mysqli_real_escape_string($conn, $_POST['FirstName']);
+$ln = mysqli_real_escape_string($conn, $_POST['Surname']);
+$house = mysqli_real_escape_string($conn, $_POST['House']);
+$town = mysqli_real_escape_string($conn, $_POST['Town']);
+$county = mysqli_real_escape_string($conn, $_POST['County']);
+$country = mysqli_real_escape_string($conn, $_POST['Country']);
+$postcode = mysqli_real_escape_string($conn, $_POST['Postcode']);
 
 
 $sql = "INSERT INTO Student (studentid, dob, firstname, lastname, house, town, county, country, postcode)
